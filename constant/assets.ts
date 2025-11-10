c0c6fc59b4a6a03453c325217e177d1b68058ad7
owusuvictorjnr <owusuvictor80@gmail.com>
2025-11-10 17:18:36 +0000

    implement shopping cart

---
+++ constant/assets.ts
@@ -0,0 +1,42 @@
+// Copyright 2025 vitech
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+export const SEARCH_ICON = "/assets/svg/search.svg";
+export const SHOPPING_BAG_ICON = "/assets/svg/shopping-bag.svg";
+
+export const IMG1 = "/assets/img/img1.jpg";
+export const IMG2 = "/assets/img/img2.jpg";
+export const IMG3 = "/assets/img/img3.jpg";
+export const IMG4 = "/assets/img/img4.jpg";
+export const IMG5 = "/assets/img/img5.jpg";
+export const IMG6 = "/assets/img/img6.jpg";
+export const IMG7 = "/assets/img/img7.jpg";
+export const IMG8 = "/assets/img/img8.jpg";
+export const IMG9 = "/assets/img/img9.jpg";
+export const IMG10 = "/assets/img/img10.jpg";
+export const IMG11 = "/assets/img/img11.jpg";
+export const IMG12 = "/assets/img/img12.jpg";
+export const IMG13 = "/assets/img/img13.jpg";
+export const IMG15 = "/assets/img/img15.jpg";
+export const IMG16 = "/assets/img/img16.jpg";
+export const IMG17 = "/assets/img/img17.jpg";
+export const IMG18 = "/assets/img/img18.jpg";
+export const IMG19 = "/assets/img/img19.jpg";
+export const IMG20 = "/assets/img/img20.jpg";
+export const IMG21 = "/assets/img/img21.jpg";
+export const IMG22 = "/assets/img/img22.jpg";
+export const IMG23 = "/assets/img/img23.jpg";
+export const IMG24 = "/assets/img/img24.jpg";
+export const IMG25 = "/assets/img/img25.jpg";
+export const IMG26 = "/assets/img/img26.jpg";
+export const IMG27 = "/assets/img/img27.jpg";
