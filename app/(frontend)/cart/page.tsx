1f85bdd71f7d39f0e1b6dae04168e879b3446d20
owusuvictorjnr <owusuvictor80@gmail.com>
2025-11-10 17:12:42 +0000

    cart

---
+++ app/(frontend)/cart/page.tsx
@@ -0,0 +1,29 @@
+// Copyright 2025 vitech
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { SHOPPING_BAG_ICON } from "@/constant/assets";
+import Image from "next/image";
+
+export default function Cart() {
+  return (
+    <div className="flex justify-end items-center px-8 pb-2">
+      <Image
+        src={SHOPPING_BAG_ICON}
+        alt="cart icon"
+        width={30}
+        height={30}
+        
+      />
+    </div>
+  );
+}
