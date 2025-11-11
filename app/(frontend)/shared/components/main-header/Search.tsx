7117c7153a35de3dd9e81f96ecdb6572244ee3c9
owusuvictorjnr <owusuvictor80@gmail.com>
2025-11-11 11:37:49 +0000

    use query

--- app/(frontend)/shared/components/main-header/Search.tsx
+++ app/(frontend)/shared/components/main-header/Search.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Copyright 2025 vitech
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,14 +12,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { SEARCH_ICON } from "@/constant/assets";
+import { useSearchProducts } from "@/features/products/hooks/queries/useSearchProducts";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Search() {
+  const [query, setQuery] = useState("");
+  const { data } = useSearchProducts(query);
   return (
-    <div className="relative">
-      <div className="absolute right-0 top-0 mt-2 mr-2 order-2">
+    <div className="">
+      {/* <div className="absolute right-0 top-0 mt-2 mr-2 order-2">
         <Image
           src={SEARCH_ICON}
           alt="Search"
@@ -26,13 +34,27 @@ export default function Search() {
           width={20}
           className="bg--500 "
         />
-      </div>
-      <div className="w-100 md:w-160 pl-32">
+      </div> */}
+      <div className="relative  w-full max-w-md">
         <Input
           type="text"
-          className="border-3 active:border-red-400 bg-gray-400"
-          placeholder="Search nasty perfumes..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="border px-4 py-2 w-full"
+          placeholder="Search  perfumes..."
         />
+        {query && data && (
+          <ul className="absolute bg-white border w-full rounded-lg mt-2 z-10">
+            {data.map((product: any) => (
+              <li
+                key={product.id}
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+              >
+                {product.name}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
