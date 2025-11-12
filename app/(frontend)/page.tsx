99ed670a3bf8f7cb6dea25edf8685f65d5802a30
owusuvictorjnr <owusuvictor80@gmail.com>
2025-11-12 17:23:51 +0000

    import ProductList component

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,21 +1,7 @@
-// Copyright 2025 vitech
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 "use client";
 
 import { Hero } from "@/app/(frontend)/shared/components/Hero";
-
+import ProductList from "@/app/(frontend)/shop/page";
 
 export default function Homepage() {
   return (
@@ -23,6 +9,11 @@ export default function Homepage() {
       {/* Hero Carousel */}
       <div className="">
         <Hero />
+      </div>
+
+      {/* Featured Products */}
+      <div className="">
+        <ProductList />
       </div>
     </div>
   );
